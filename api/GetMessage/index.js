8bfabeade3700ffc2ec4d008839c737787fb2916
Vivek Jilla <vivekjilla7@gmail.com>
2022-10-17 18:25:18 +0530

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,7 +1,8 @@
 module.exports = async function (context, req) {
+  
+  const value = process.env.Zoro;
+  const responseMessage = "Zoro:"+value;
   context.res = {
-    const value = process.env.Zoro;
-    const responseMessage = "Zoro:"+value;
     // status: 200, /* Defaults to 200 */
     body: { text: "Hello from the API, "+ responseMessage },
   };
