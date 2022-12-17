7f443c1b70a0a921ad66efe21904f7e66d209b0e
Vivek Jilla <vivekjilla7@gmail.com>
2022-12-17 06:39:40 +0530

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req) {
   context.res = {
     // status: 200, /* Defaults to 200 */
-    body: { text: "Hello from the API "+ process.env.USER_ID + "," + process.env.Zoro + "," + process.env.hello },
+    body: { text: "Hello from the API PR env "+ process.env.USER_ID + "," + process.env.Zoro + "," + process.env.hello },
   };
 };
